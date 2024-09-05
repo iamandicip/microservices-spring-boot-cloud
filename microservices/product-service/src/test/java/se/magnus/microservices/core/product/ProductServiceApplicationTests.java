abc71e8ab40900db132ff41c883b9828ae9f1e7c
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2024-09-05 23:25:33 +0200

    Initial commit

---
+++ microservices/product-service/src/test/java/se/magnus/microservices/core/product/ProductServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package se.magnus.microservices.core.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
