4c08cf50f73bafaa0e7568bbd138a5f93b590577
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2024-09-15 00:22:56 +0200

    linked all services inside composite service added integration tests

--- microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,13 +1,29 @@
 package se.magnus.microservices.composite.product;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+//	@Bean
+//	public ObjectMapper objectMapper() {
+//		return new ObjectMapper();
+//	}
 }
