74dde7b2dc0e9dfd1f040cef95b38955688fa0be
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2024-10-01 23:14:01 +0200

    made some more progress on adding new api endpoints

--- api/src/main/java/se/magnus/api/core/recommendation/Recommendation.java
+++ api/src/main/java/se/magnus/api/core/recommendation/Recommendation.java
@@ -1,59 +1,83 @@
 package se.magnus.api.core.recommendation;
 
 public class Recommendation {
-  private final int productId;
-  private final int recommendationId;
-  private final String author;
-  private final int rate;
-  private final String content;
-  private final String serviceAddress;
+    private int productId;
+    private int recommendationId;
+    private String author;
+    private int rate;
+    private String content;
+    private String serviceAddress;
 
-  public Recommendation() {
-    productId = 0;
-    recommendationId = 0;
-    author = null;
-    rate = 0;
-    content = null;
-    serviceAddress = null;
-  }
+    public Recommendation() {
+        productId = 0;
+        recommendationId = 0;
+        author = null;
+        rate = 0;
+        content = null;
+        serviceAddress = null;
+    }
 
-  public Recommendation(
-    int productId,
-    int recommendationId,
-    String author,
-    int rate,
-    String content,
-    String serviceAddress) {
+    public Recommendation(
+            int productId,
+            int recommendationId,
+            String author,
+            int rate,
+            String content,
+            String serviceAddress) {
 
-    this.productId = productId;
-    this.recommendationId = recommendationId;
-    this.author = author;
-    this.rate = rate;
-    this.content = content;
-    this.serviceAddress = serviceAddress;
-  }
+        this.productId = productId;
+        this.recommendationId = recommendationId;
+        this.author = author;
+        this.rate = rate;
+        this.content = content;
+        this.serviceAddress = serviceAddress;
+    }
 
-  public int getProductId() {
-    return productId;
-  }
+    public int getProductId() {
+        return productId;
+    }
 
-  public int getRecommendationId() {
-    return recommendationId;
-  }
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 
-  public String getAuthor() {
-    return author;
-  }
+    public int getRecommendationId() {
+        return recommendationId;
+    }
 
-  public int getRate() {
-    return rate;
-  }
+    public void setRecommendationId(int recommendationId) {
+        this.recommendationId = recommendationId;
+    }
 
-  public String getContent() {
-    return content;
-  }
+    public String getAuthor() {
+        return author;
+    }
 
-  public String getServiceAddress() {
-    return serviceAddress;
-  }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getServiceAddress() {
+        return serviceAddress;
+    }
+
+    public void setServiceAddress(String serviceAddress) {
+        this.serviceAddress = serviceAddress;
+    }
 }
--- microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
@@ -1,47 +1,70 @@
 package se.magnus.microservices.core.review.services;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.RestController;
 import se.magnus.api.core.review.Review;
 import se.magnus.api.core.review.ReviewService;
 import se.magnus.api.exceptions.InvalidInputException;
+import se.magnus.microservices.core.review.persistence.ReviewEntity;
+import se.magnus.microservices.core.review.persistence.ReviewRepository;
 import se.magnus.util.http.ServiceUtil;
+
+import java.util.List;
 
 @RestController
 public class ReviewServiceImpl implements ReviewService {
 
-  private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
+    private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
 
-  private final ServiceUtil serviceUtil;
+    private final ReviewRepository repository;
 
-  @Autowired
-  public ReviewServiceImpl(ServiceUtil serviceUtil) {
-    this.serviceUtil = serviceUtil;
-  }
+    private final ReviewMapper mapper;
 
-  @Override
-  public List<Review> getReviews(int productId) {
+    private final ServiceUtil serviceUtil;
 
-    if (productId < 1) {
-      throw new InvalidInputException("Invalid productId: " + productId);
+    @Autowired
+    public ReviewServiceImpl(ReviewRepository repository, ReviewMapper mapper, ServiceUtil serviceUtil) {
+        this.repository = repository;
+        this.mapper = mapper;
+        this.serviceUtil = serviceUtil;
     }
 
-    if (productId == 213) {
-      LOG.debug("No reviews found for productId: {}", productId);
-      return new ArrayList<>();
+    @Override
+    public Review createReview(Review body) {
+        try {
+            ReviewEntity entity = mapper.apiToEntity(body);
+            ReviewEntity newEntity = repository.save(entity);
+
+            LOG.debug("createReview: created a review entity: {}/{}", body.getProductId(), body.getReviewId());
+            return mapper.entityToApi(newEntity);
+
+        } catch (DataIntegrityViolationException dive) {
+            throw new InvalidInputException("Duplicate key, Product Id: " + body.getProductId() + ", Review Id:" + body.getReviewId());
+        }
     }
 
-    List<Review> list = new ArrayList<>();
-    list.add(new Review(productId, 1, "Author 1", "Subject 1", "Content 1", serviceUtil.getServiceAddress()));
-    list.add(new Review(productId, 2, "Author 2", "Subject 2", "Content 2", serviceUtil.getServiceAddress()));
-    list.add(new Review(productId, 3, "Author 3", "Subject 3", "Content 3", serviceUtil.getServiceAddress()));
+    @Override
+    public List<Review> getReviews(int productId) {
 
-    LOG.debug("/reviews response size: {}", list.size());
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
 
-    return list;
-  }
+        List<ReviewEntity> entityList = repository.findByProductId(productId);
+        List<Review> list = mapper.entityListToApiList(entityList);
+        list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
+
+        LOG.debug("getReviews: response size: {}", list.size());
+
+        return list;
+    }
+
+    @Override
+    public void deleteReviews(int productId) {
+        LOG.debug("deleteReviews: tries to delete reviews for the product with productId: {}", productId);
+        repository.deleteAll(repository.findByProductId(productId));
+    }
 }
