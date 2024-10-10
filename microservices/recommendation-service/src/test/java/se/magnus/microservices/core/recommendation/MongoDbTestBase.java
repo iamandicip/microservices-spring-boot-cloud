703677a3771777043c27a0d429fa94ddc508398a
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2024-10-10 23:51:14 +0200

    Added more functionality and tests for reactive capabilities and event based communication

--- microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/MongoDbTestBase.java
+++ microservices/recommendation-service/src/test/java/se/magnus/microservices/core/recommendation/MongoDbTestBase.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
 
 public abstract class MongoDbTestBase {
-    private static MongoDBContainer database = new MongoDBContainer("mongo:6.0.4");
+    private static final MongoDBContainer database = new MongoDBContainer("mongo:6.0.4");
 
     static {
         database.start();
@@ -13,7 +13,7 @@ public abstract class MongoDbTestBase {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.data.mongodb.host", database::getContainerIpAddress);
+        registry.add("spring.data.mongodb.host", database::getHost);
         registry.add("spring.data.mongodb.port", () -> database.getMappedPort(27017));
         registry.add("spring.data.mongodb.database", () -> "test");
     }
